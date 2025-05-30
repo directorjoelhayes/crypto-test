baadb6d221bb4d059028a8ac5394c64827d5897b
Joel Hayes <directorjoelhayes@gmail.com>
2025-05-30 10:01:44 -0400

    built the prototype

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,297 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import { reactive } from 'vue'
+import { onMounted, reactive, ref } from "vue";
+import { ulid } from "ulid";
 
-import AES from 'crypto-js/aes';
-import { enc } from 'crypto-js';
+const dropBox = reactive({
+  items: new Map(),
+  nonces: new Map(),
+  receipts: new Map(),
+  public: null,
+  private: null,
 
-const message = 'Hello, world!';
-const privateKey = '1234567890';
+  addItem(key, item, nonce) {
+    this.items.set(key, item);
+    this.nonces.set(key, nonce);
+  },
+  removeItem(key) {
+    this.items.delete(key);
+    this.nonces.delete(key);
+  },
+  async addReceipt(receipt) {
+    //decode the receipt
+    const decryptedReceipt = await SEA.decrypt(
+      receipt,
+      await SEA.secret(dropBox.private.a, dropBox.private.b)
+    );
 
-// Encrypt
-const encrypted = AES.encrypt(message, privateKey).toString();
+    const itemNonce = this.nonces.get(decryptedReceipt.itemKey);
 
-// Decrypt
-const decrypted = AES.decrypt(encrypted, privateKey).toString(enc.Utf8);
-
-
-const createNode = ({
-  name,
-  to,
-  toSecret, 
-  fromSecret
-} = {}) => {
-
-  const sendMessage = (message) => {
-    const encrypted = AES.encrypt(message, toSecret).toString();
-    return {
-      to,
-      message: encrypted
+    if (itemNonce !== decryptedReceipt.nonce) {
+      console.log("nonce mismatch");
+      return;
     }
-  }
 
-  const receiveMessage = (message) => {
-    const decrypted = AES.decrypt(message, fromSecret).toString(enc.Utf8);
-    return decrypted;
-  }
+    const receiptId = ulid();
 
+    this.receipts.set(receiptId, decryptedReceipt);
+  },
+  async initialize() {
+    const boxPair = await SEA.pair();
+    this.public = {
+      a: boxPair.epub,
+      b: boxPair,
+    };
+    this.private = {
+      a: boxPair.epub,
+      b: boxPair,
+    };
+  },
+});
+
+async function makePair() {
+  const key1 = await SEA.pair();
+  const key2 = await SEA.pair();
   return {
-    name,
-    to,
-    toSecret, 
-    fromSecret,
-    sendMessage,
-    receiveMessage
-  }
+    public: {
+      a: key1.epub,
+      b: key2,
+    },
+    private: {
+      a: key2.epub,
+      b: key1,
+    },
+  };
 }
 
-const step = reactive({
-  encrypted: encrypted,
-  decrypted: decrypted
-})
+async function makeNode(onFinalCallback) {
+  const node = await makePair();
+  const id = ulid();
+  return {
+    public: node.public,
+    private: node.private,
+    id,
+    scanInterval: null,
+    scan() {
+      this.scanInterval = setInterval(async () => {
+        const items = dropBox.items;
+        for (const [key, item] of items) {
+          const decryptedKey = await SEA.decrypt(
+            key,
+            await SEA.secret(node.private.a, node.private.b)
+          );
+          if (!decryptedKey) continue;
+
+          if (decryptedKey.startsWith("address:")) {
+            this.stopScan();
+            // dropBox.addReceipt(pickupReceipt);
+
+            const itemCopy = item;
+            const dataAggregator = [];
+
+            const decryptedItem = await SEA.decrypt(
+              itemCopy,
+              await SEA.secret(node.private.a, node.private.b)
+            );
+
+            const pickupNonce = decryptedItem.nonce;
+            const publicKey = decryptedItem.payloadSecret;
+
+            const pickupReceipt = await SEA.encrypt(
+              JSON.stringify({
+                publicKey: await SEA.encrypt(
+                  "pickup at: " + new Date().toISOString(),
+                  await SEA.secret(publicKey.a, publicKey.b)
+                ),
+                action: "pickup",
+                itemKey: key,
+                nonce: pickupNonce,
+                timestamp: Date.now(),
+              }),
+              await SEA.secret(dropBox.public.a, dropBox.public.b)
+            );
+
+            dropBox.addReceipt(pickupReceipt);
+
+            let foundAddress = false;
+
+            for (const address of decryptedItem.addresses) {
+              const decryptedAddress = await SEA.decrypt(
+                address,
+                await SEA.secret(node.private.a, node.private.b)
+              );
+              if (!decryptedAddress) continue;
+
+              if (decryptedAddress.startsWith("address:")) {
+                foundAddress = true;
+                await passOnPayload(decryptedAddress, decryptedItem);
+              }
+            }
+
+            if (!foundAddress) {
+              console.log("Last node reached:", this.id);
+              this.onFinal(decryptedItem.data, node);
+            }
+
+            async function passOnPayload(decryptedAddress, decryptedItem) {
+              const publicKey = JSON.parse(
+                decryptedAddress.split("address:")[1]
+              );
+              const address = await SEA.encrypt(
+                "address:" + ulid(),
+                await SEA.secret(publicKey.a, publicKey.b)
+              );
+
+              const addedData = id;
+
+              const payloadSecret = decryptedItem.payloadSecret;
+
+              const encryptedAddedData = await SEA.encrypt(
+                addedData,
+                await SEA.secret(payloadSecret.a, payloadSecret.b)
+              );
+
+              const data = [...decryptedItem.data, encryptedAddedData];
+
+              console.log(data, "data");
+
+              const newNonce = ulid();
+
+              const encryptedPayload = await SEA.encrypt(
+                JSON.stringify({
+                  ...decryptedItem,
+                  data: data,
+                  nonce: newNonce,
+                }),
+                await SEA.secret(publicKey.a, publicKey.b)
+              );
+              dropBox.addItem(address, encryptedPayload, newNonce);
+            }
+          }
+        }
+      }, 1000);
+    },
+    stopScan() {
+      clearInterval(this.scanInterval);
+    },
+    async send(address, payload) {
+      const encrypted = await SEA.encrypt(
+        payload,
+        await SEA.secret(this.private.a, this.private.b)
+      );
+      dropBox.addItem(address, encrypted);
+    },
+    onFinal(data, node) {
+      onFinalCallback(data, node);
+    },
+  };
+}
+
+const finalData = ref("");
+
+onMounted(async () => {
+  await dropBox.initialize();
+
+  let starterNode = "";
+  
+  starterNode = await makeNode(async (data, node) => {
+    console.log(node, starterNode, "node");
+    
+    // Use Promise.all to wait for all decryption operations
+    const decryptedItems = await Promise.all(data.map(async (item) => {
+      return await SEA.decrypt(
+        item,
+        await SEA.secret(node.private.a, node.private.b)
+      );
+    }));
+
+    console.log(decryptedItems, "decrypted items");
+    finalData.value = decryptedItems.join("");
+  });
+
+  const nodes = [
+    await makeNode(),
+    await makeNode(),
+    await makeNode(),
+    await makeNode(),
+    await makeNode(),
+    await makeNode(),
+    await makeNode(),
+    await makeNode(),
+    starterNode,
+  ];
+
+  const encryptedAddresses = await Promise.all(
+    nodes.map(async (node, index) => {
+      if (index < nodes.length - 1) {
+        return await SEA.encrypt(
+          "address:" + JSON.stringify(nodes[index + 1].public),
+          await SEA.secret(node.public.a, node.public.b)
+        );
+      }
+      return null;
+    })
+  );
+
+  const validAddresses = encryptedAddresses.filter(
+    (address) => address !== null
+  );
+  const nonce = ulid();
+
+  const payload = {
+    addresses: validAddresses,
+    payloadSecret: starterNode.public,
+    data: [],
+    nonce,
+  };
+
+  console.log(payload, "payload!!");
+
+  const encryptedPayload = await SEA.encrypt(
+    JSON.stringify(payload),
+    await SEA.secret(nodes[0].public.a, nodes[0].public.b)
+  );
+
+  const encryptedAddress = await SEA.encrypt(
+    "address:" + ulid(),
+    await SEA.secret(nodes[0].public.a, nodes[0].public.b)
+  );
+
+  dropBox.addItem(encryptedAddress, encryptedPayload, nonce);
+
+  nodes.map((node) => {
+    node.scan();
+  });
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Hello World</h1>
+    <div class="receipts">
+      <div v-for="receipt in dropBox.receipts" :key="receipt">
+        {{ receipt[0] }} : {{ receipt[1].nonce }}
+      </div>
+    </div>
+    <div class="final-data">
+      {{ finalData }}
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  {{ step }}
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.receipts {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.final-data {
+    position: relative;
+    overflow: hidden; 
+    width: 500px;
+    overflow-wrap: break-word;
+    background-color: #af3248;
+    border-radius: 20px;
+    padding: 20px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
